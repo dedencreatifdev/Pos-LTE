fc9345e4e22cfe297eac99656388e49520c5bf2a
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-03-28 03:22:53 +0700

    .

--- resources/views/livewire/admin/produk/produk-edit.blade.php
+++ resources/views/livewire/admin/produk/produk-edit.blade.php
@@ -1,3 +1,16 @@
 <div>
-    123456
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="p-2 card-body">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control form-control-sm" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
