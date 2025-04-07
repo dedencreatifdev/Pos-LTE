1845f5aae9e9509bf14220346fd493c4df98ff22
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-04-07 18:14:33 +0700

    .

--- resources/views/livewire/dashboard/dashboard-index.blade.php
+++ resources/views/livewire/dashboard/dashboard-index.blade.php
@@ -1,7 +1,7 @@
 <div>
     <!-- Info boxes -->
     <div class="row">
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-6 col-sm-6 col-md-3">
             <div class="info-box">
                 <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
 
@@ -17,7 +17,7 @@
             <!-- /.info-box -->
         </div>
         <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-6 col-sm-6 col-md-3">
             <div class="info-box mb-3">
                 <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
 
@@ -34,7 +34,7 @@
         <!-- fix for small devices only -->
         <div class="clearfix hidden-md-up"></div>
 
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-6 col-sm-6 col-md-3">
             <div class="info-box mb-3">
                 <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
 
@@ -47,7 +47,7 @@
             <!-- /.info-box -->
         </div>
         <!-- /.col -->
-        <div class="col-12 col-sm-6 col-md-3">
+        <div class="col-6 col-sm-6 col-md-3">
             <div class="info-box mb-3">
                 <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
