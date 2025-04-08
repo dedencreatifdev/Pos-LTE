aabafe6972db3ebe34c265efeea3d7c42eaf2ae7
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-04-08 18:05:54 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(DashboardController::class)->group(function () {
+    Route::get('/', 'index')->name('dashboard.index');
 });
