d11c84ce40e9e69fd3500654b827810b67691fd3
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-04-08 17:54:06 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
-use App\Livewire\Dashboard\DashboardIndex;
-use App\Livewire\Produk\ProdukIndex;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',DashboardIndex::class)->name('dashboard.index');
-Route::get('/produk',ProdukIndex::class)->name('produk.index');
+Route::get('/', function () {
+    return view('welcome');
+});
